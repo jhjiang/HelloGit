0c524604d77ac580ed6fd20e548459672c1e1a02
Jett Jiang <JJHJett@JetttekiMacBook-Pro.local>
2015-09-09 19:45:41 +0800

    nslog

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"GitTest");
+    NSLog(@"GitTest1");
 }
 
 - (void)didReceiveMemoryWarning {
